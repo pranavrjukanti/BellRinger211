37d470258b0c46ee927dc9f457d905c05c0b41e5
pranavrjukanti <157426842+pranavrjukanti@users.noreply.github.com>
2025-02-11 13:40:13 -0800

    Create pranav.py
    bell ringer attempt 2/11/25

---
+++ pranav.py
@@ -0,0 +1,7 @@
+start_min = int(input("how many minutes since class started")) 
+
+min_left = 59 - start_min
+
+min_left = str(min_left)
+
+print("Now there are",min_left,"minutes left in our CSP class")
